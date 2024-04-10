6d63f9fe76642c8be8be830f55a88cc68362693e
BlakeOne <30273164+BlakeOne@users.noreply.github.com>
2024-04-10 15:23:12 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,21 @@
+class NodePresets():
+    OUTPUT_NODE = True
+    FUNCTION = "func"
+    CATEGORY = "NodePresets"
+    RETURN_TYPES = ()
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "name": ("STRING", {"name": ""}),
+            },
+        }
+
+    RETURN_TYPES = ("*",)
+
+NODE_CLASS_MAPPINGS = { "NodePresets": NodePresets }
+
 WEB_DIRECTORY = "./js"
-__all__ = ["WEB_DIRECTORY"]
+
+__all__ = ["NODE_CLASS_MAPPINGS", "WEB_DIRECTORY"]
