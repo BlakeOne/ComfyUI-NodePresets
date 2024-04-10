61a72f73afdf39a274d629155286cdf1957c5698
BlakeOne <30273164+BlakeOne@users.noreply.github.com>
2024-04-10 15:39:44 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,11 @@
 class NodePresets():
     @classmethod
     def INPUT_TYPES(s):
-        return {
-            "required": {
-                "name": ("STRING", {"name": ""}),
-            },
-        }
-        
-    OUTPUT_NODE = True
+        return { "required": {) }
+    
+    RETURN_TYPES = ("*",)
+    CATEGORY = "NodePresets"    
     FUNCTION = "func"
-    CATEGORY = "NodePresets"
-    RETURN_TYPES = ("*",)    
 
 NODE_CLASS_MAPPINGS = { "NodePresets": NodePresets }
 
